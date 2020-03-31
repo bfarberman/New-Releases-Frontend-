88b431bbf4f48a998441f2225d96b76c891aaa8e
bfarberman <bfarberman@gmail.com>
2020-03-30 22:32:31 -0400

    trying heroku

---
+++ build/precache-manifest.c7fa6ef3dbe2fe6caec679b8c8fa1eed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde2ad4da7f6d0f90e7a7a42535e6ad3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6f7e8c2e26e71fceded",
+    "url": "/static/css/main.1ebdbbe6.chunk.css"
+  },
+  {
+    "revision": "4986f8e571fc1eac3342",
+    "url": "/static/js/2.d3244c8f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d3244c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f7e8c2e26e71fceded",
+    "url": "/static/js/main.04336dfa.chunk.js"
+  },
+  {
+    "revision": "3f8f904b86deb5ea8e53",
+    "url": "/static/js/runtime-main.ecee52d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
